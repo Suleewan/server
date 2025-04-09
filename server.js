85b51945c8004b69f67baa49ee3b3396d86aa9b8
Suleewan <165354516+Suleewan@users.noreply.github.com>
2025-04-09 19:08:14 +0700

    first commit

--- server.js
+++ server.js
@@ -1,29 +1,58 @@
-//import ....
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const {readdirSync} = require('fs')
-const cors = require('cors')
+// //import ....
+// const express = require('express')
+// const app = express()
+// const morgan = require('morgan')
+// const {readdirSync} = require('fs')
+// const cors = require('cors')
+// const { PrismaClient } = require('@prisma/client');
+// const prisma = new PrismaClient();
+// const bodyParser = require('body-parser');
+
+
+
+
+
+// app.use(morgan('dev'))
+// app.use(express.json());
+// app.use(cors())
+// app.use(bodyParser.json());
+
+
+
+// readdirSync('./routes')
+// .map((c)=> app.use('/api', require('./routes/'+ c)) )
+
+
+
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// prisma.$connect()
+//   .then(() => {
+//     console.log('Connected to the database');
+//   })
+//   .catch((error) => {
+//     console.error('Database connection failed:', error);
+// });
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { readdirSync } = require('fs');
+const cors = require('cors');
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
 const bodyParser = require('body-parser');
+const prisma = new PrismaClient();
 
-
-
-
-
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-
-
-readdirSync('./routes')
-.map((c)=> app.use('/api', require('./routes/'+ c)) )
-
-
-
+readdirSync('./routes').map((c) => app.use('/api', require('./routes/' + c)));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
@@ -36,4 +65,4 @@ prisma.$connect()
   })
   .catch((error) => {
     console.error('Database connection failed:', error);
-});
+  });
