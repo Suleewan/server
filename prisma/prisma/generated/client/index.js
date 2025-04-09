35e9a200022a29c23b365d9c926363144ba0c6a5
Suleewan <165354516+Suleewan@users.noreply.github.com>
2025-04-09 19:45:27 +0700

    first commit

--- prisma/prisma/generated/client/index.js
+++ prisma/prisma/generated/client/index.js
@@ -258,11 +258,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://iseus_user:F46W0OMJcSrpgSdTVjECVVhjT2CVa2R0@dpg-cvr6emi4d50c738e6pj0-a.singapore-postgres.render.com/iseus"
+        "value": null
       }
     }
   },
